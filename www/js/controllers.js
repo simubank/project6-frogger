e49254cdc990f06cf87672b68217f51e01d807dd
vickyduarte <vicky.c.duarte@gmail.com>
2018-07-18 21:56:21 -0400

    Added in fucntions for cancel and working on submit...

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,34 +28,10 @@ function ($scope, $stateParams) {
   function ($scope, $stateParams, BotsService, User) {
     var vm=this;
     $scope.currentUserId = 0;
-    $scope.user = {
-        "name": "Jon Snow", 
-        "id": 0,
-        "location": "Toronto, Ontario",
-        "description": "Hi! I am currently looking for a place to stay for the upcoming winter!Hi! I am currently looking for a place to stay for the upcoming winter!Hi! I am currently looking for a place to stay for the upcoming winter!",
-        "status": "Seeking a 1 year lease in Guelph. Subletting a room in Kitchener for Fall term",
-        "listings": [],
-        "reviews": [
-            {
-                "review" : "Jon was a very great tenant, who is clean, quiet and is a real leader! 10/10 would lease to again.",
-                "name": "Ned Stark",
-                "id": 3000,
-                "timestamp": "July 15, 2018",
-                "rating": 5.0
-            },
-            {
-                "review" : "An amazing tenant with a great track record...",
-                "name": "Daeneyrs Targaryon",
-                "id": 1000,
-                "timestamp": "June 17, 2017",
-                "rating": 4.0
-            }
-        ]
-    };
 
     BotsService.getUser(0).then(function(data) {
-    $scope.user = new User(data.result[0]);
-    $scope.averageStars = $scope.createStars($scope.user.averageStars());
+        $scope.user = new User(data.result[0]);
+        $scope.averageStars = $scope.createStars($scope.user.averageStars());
     });
     
     $scope.createStars = function(rating){
@@ -96,10 +72,10 @@ function ($scope, $stateParams) {
 }])
 
 
-.controller('writeReviewCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('writeReviewCtrl', ['$rootScope', '$scope', '$stateParams', '$state', 'BotsService',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($rootScope, $scope, $stateParams, $state, BotsService) {
     $scope.starRatingClass =["far fa-star","far fa-star","far fa-star","far fa-star","far fa-star"];
 
     $scope.starHighlight = function(starCount){
@@ -116,6 +92,25 @@ function ($scope, $stateParams) {
         return $scope.starRatingClass;
     }
 
+    $scope.previousState;
+    $scope.currentState;
+
+    $rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
+        $scope.previousState = from.name;
+        $scope.currentState = to.name;
+    });
+
+    $scope.cancel= function(){
+        $state.go($scope.previousState);
+    }
+
+    BotsService.getUser(0).then(function(data) {
+        $scope.user = new User(data.result[0]);
+    });
+
+    $scope.submitReview= function(){
+        $scope.user.reviews.push($scope.review-text)
+    }
 
 }])
    
